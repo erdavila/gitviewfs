a514b6559c2423a9f2143affdbbcec7169313eff
Eduardo R. D'Avila <erdavila@gmail.com>
2012-11-04 17:03:10 -0200

    Implement parsing of commit date

--- git_objects_parser.py
+++ git_objects_parser.py
@@ -1,9 +1,10 @@
 from collections import namedtuple
 import re
 import subprocess
+import time
 
 
-Commit = namedtuple('Commit', 'author_name, author_email, message')
+Commit = namedtuple('Commit', 'author_name, author_email, author_date, message')
 
 def parse_git_commit(commit):
 	commit_content = subprocess.check_output(['git', 'cat-file', 'commit', commit])
@@ -25,11 +26,30 @@ def parse_git_commit_content(commit_content):
 	return Commit(message=message, **values)
 
 def parse_git_commit_header(line):
-	m = re.match(r'author (.+) <(.+)>', line)
+	m = re.match(r'author (.+) <(.+)> (.+)', line)
 	if m is not None:
 		return {
-			'author_name' : m.group(1),
+			'author_name'  : m.group(1),
 			'author_email' : m.group(2),
+			'author_date'  : parse_git_commit_date(m.group(3))
 		}
 	
 	return {}
+
+def parse_git_commit_date(commit_date):
+	timestamp, tz = commit_date.split(' ')
+	timestamp = int(timestamp)
+	timestamp += parse_tz_to_seconds(tz)
+	date_string = time.asctime(time.gmtime(timestamp))
+	return date_string + ' ' + tz
+
+def parse_tz_to_seconds(tz):
+	m = re.match(r'^(\-|\+)(\d\d)(\d\d)$', tz)
+	signal = m.group(1)
+	hours = int(m.group(2))
+	minutes = int(m.group(3))
+	
+	value_in_seconds = 60*(60*hours + minutes)
+	if signal == '-':
+		value_in_seconds = -value_in_seconds
+	return value_in_seconds
--- tests/test_git_objects_parser.py
+++ tests/test_git_objects_parser.py
@@ -1,12 +1,15 @@
 import unittest
 
-from git_objects_parser import parse_git_commit_content
+from git_objects_parser import parse_git_commit_content, parse_git_commit_date,\
+	parse_tz_to_seconds
 
 
 class TestGitObjectsParser(unittest.TestCase):
 	
 	AUTHOR_NAME = 'Author Name'
 	AUTHOR_EMAIL = 'author@domain1.com'
+	AUTHOR_DATE = '1350852983 -0700'
+	AUTHOR_DATE_FORMATED = 'Sun Oct 21 13:56:23 2012 -0700'
 	MESSAGE_LINES = ( 
 		'This is the first line',
 		'',
@@ -20,8 +23,8 @@ class TestGitObjectsParser(unittest.TestCase):
 				'tree 0123456789012345678901234567890123456789',
 				'parent abcdefabcdefabcdefabcdefabcdefabcdefabcd',
 				'parent a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2',
-				'author ' + self.AUTHOR_NAME + ' <' + self.AUTHOR_EMAIL + '> 123456789 +0200',
-				'committer Name of Committer <committer@domain2.net> 987654321 -0300',
+				'author ' + self.AUTHOR_NAME + ' <' + self.AUTHOR_EMAIL + '> ' + self.AUTHOR_DATE,
+				'committer Name of Committer <committer@domain2.net> 987654321 +0300',
 				'',
 			) + self.MESSAGE_LINES
 		)
@@ -34,6 +37,28 @@ class TestGitObjectsParser(unittest.TestCase):
 		commit = parse_git_commit_content(self.commit_content)
 		self.assertEqual(self.AUTHOR_EMAIL, commit.author_email)
 
+	def test_parse_commit_author_date(self):
+		commit = parse_git_commit_content(self.commit_content)
+		self.assertEqual(self.AUTHOR_DATE_FORMATED, commit.author_date)
+	
+	def test_commit_date_1(self):
+		commit_date = self.AUTHOR_DATE
+		parsed_date = parse_git_commit_date(commit_date)
+		self.assertEqual(self.AUTHOR_DATE_FORMATED, parsed_date)
+	
+	def test_commit_date_2(self):
+		commit_date = '1350507449 +0100'
+		parsed_date = parse_git_commit_date(commit_date)
+		self.assertEqual('Wed Oct 17 21:57:29 2012 +0100', parsed_date)
+	
+	def test_tz_parsing_1(self):
+		seconds = parse_tz_to_seconds('-0700')
+		self.assertEqual(-7 * 60 * 60, seconds)
+	
+	def test_tz_parsing_2(self):
+		seconds = parse_tz_to_seconds('+1234')
+		self.assertEqual(12*60*60 + 34*60, seconds)
+	
 	def test_parse_commit_message(self):
 		commit = parse_git_commit_content(self.commit_content)
 		self.assertEqual(self.join_lines(self.MESSAGE_LINES), commit.message)
